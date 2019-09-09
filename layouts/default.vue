7a4a56f3c280c025ad45ceddf90615e1231ca7d7
Hannes Wylaers <hwylaers@gmail.com>
2019-09-09 20:02:14 +0200

    Reorder/cleanup.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,27 +1,15 @@
 <template>
   <div>
     <navbar />
-    <slideshow />
-    <expertises />
-    <callToAction />
-    <contact />
   </div>
 </template>
 
 <script>
 import navbar from '../components/Navbar.vue'
-import expertises from '../components/Expertises.vue'
-import slideshow from '../components/Slideshow.vue'
-import callToAction from '../components/Calltoaction.vue'
-import contact from '../components/Contact.vue'
 
 export default {
   components: {
-    navbar,
-    expertises,
-    slideshow,
-    callToAction,
-    contact
+    navbar
   }
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -22,5 +22,5 @@ export default {
 }
 </script>
 <style lang="scss">
-@import '../assets/variables.scss';
+@import '../assets/scss/variables';
 </style>
